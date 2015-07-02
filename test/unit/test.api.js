5caf51092b159f1060962b2391c72cf21fad0064
wuchangming <wuchangmingnice@163.com>
2015-07-03 02:53:19 +0800

    add expires test

--- test/unit/test.api.js
+++ test/unit/test.api.js
@@ -18,7 +18,7 @@ var testDataTypes = {
         [{a: 1}, {b: 1}, {c: 'a'}],
         [1, 'asdf', 3.23, false, {ss: 'asdf'}]
     ]
-}
+};
 
 var storage = window.localStorage;
 
@@ -59,11 +59,28 @@ describe('WebStorageCache', function() {
                             var gValue = this.wsCache.get('lWsCacheTestDataTypes');
                             expect(gValue).to.deep.equal(value);
                             expect(typeof gValue).to.be.equal(typeof value);
-                        }
+                        };
                     })(value));
                 }
             }
 
-        })
-    })
+        });
+
+        describe('expires', function() {
+            beforeEach(function() {
+                clearStorage();
+            });
+            it('should be get null when #set options: {exp: 2} after 2 seconds', function(done) {
+                this.timeout(5000);
+                var value = 'test';
+                this.wsCache.set('testExpires', value, {exp: 2});
+                expect(this.wsCache.get('testExpires')).to.be.deep.equal(value);
+                var _this = this;
+                setTimeout(function() {
+                    expect(_this.wsCache.get('testExpires') === null);
+                    done();
+                }, 2000);
+            });
+        });
+    });
 });
